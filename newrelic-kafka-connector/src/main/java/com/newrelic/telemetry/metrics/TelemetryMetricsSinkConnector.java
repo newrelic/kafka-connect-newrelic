f50c65b52a17ee697cacf0500904b415b607647d
defconx <defconx@gmail.com>
2020-08-10 11:46:33 -0400

    Formatting updates preparing for go live. Bring code into New Relic coding standard. Update documentation. Rearrange project structure.

---
+++ newrelic-kafka-connector/src/main/java/com/newrelic/telemetry/metrics/TelemetryMetricsSinkConnector.java
@@ -0,0 +1,58 @@
+package com.newrelic.telemetry.metrics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.newrelic.telemetry.TelemetrySinkConnectorConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.sink.SinkConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TelemetryMetricsSinkConnector extends SinkConnector {
+    private static Logger log = LoggerFactory.getLogger(TelemetryMetricsSinkConnector.class);
+    private TelemetrySinkConnectorConfig config;
+    private Map<String, String> configProps = new HashMap<>();
+
+    @Override
+    public String version() {
+        return "1.0.0";
+    }
+
+    @Override
+    public void start(Map<String, String> props) {
+        configProps = props;
+
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        //TODO: Return your task implementation.
+        return TelemetryMetricsSinkTask.class;
+    }
+
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+
+        final List<Map<String, String>> configs = new ArrayList<>(maxTasks);
+        for (int i = 0; i < maxTasks; ++i) {
+            configs.add(configProps);
+        }
+
+        return configs;
+
+    }
+
+    @Override
+    public void stop() {
+        //TODO: Do things that are necessary to stop your connector.
+    }
+
+    @Override
+    public ConfigDef config() {
+        return TelemetrySinkConnectorConfig.conf();
+    }
+}
---
+++ newrelic-kafka-connector/src/test/java/com/newrelic/telemetry/TelemetrySinkMetricsConnectorTest.java
@@ -0,0 +1,11 @@
+package com.newrelic.telemetry;
+
+import org.junit.Test;
+
+public class TelemetrySinkMetricsConnectorTest {
+
+  @Test
+  public void test() {
+    // Congrats on a passing test!
+  }
+}
