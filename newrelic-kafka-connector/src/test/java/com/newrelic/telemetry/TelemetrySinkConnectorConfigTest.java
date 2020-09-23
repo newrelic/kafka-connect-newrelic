2be978f4b159e3fcda4f11ddf2d27902df629f25
Rahul Bhattacharya <rahulbats@gmail.com>
2020-09-22 21:41:22 -0500

    - fixed retry logic. For things like wrong api keys or too large batch there is no need to retry - Changed some log levels from info to debug - fixed documentation - added apache license (https://github.com/newrelic/kafka-connect-newrelic/issues/23)

--- newrelic-kafka-connector/src/test/java/com/newrelic/telemetry/TelemetrySinkConnectorConfigTest.java
+++ newrelic-kafka-connector/src/test/java/com/newrelic/telemetry/TelemetrySinkConnectorConfigTest.java
@@ -8,7 +8,8 @@ public class TelemetrySinkConnectorConfigTest {
     @Test
     public void doc() {
         String stringConf = TelemetrySinkConnectorConfig.conf().toRst();
-        assertTrue(stringConf.contains("api.key"));
-
+        assertTrue(stringConf.contains(TelemetrySinkConnectorConfig.API_KEY));
+        assertTrue(stringConf.contains(TelemetrySinkConnectorConfig.RETRY_INTERVAL_MS));
+        assertTrue(stringConf.contains(TelemetrySinkConnectorConfig.MAX_RETRIES));
     }
 }
