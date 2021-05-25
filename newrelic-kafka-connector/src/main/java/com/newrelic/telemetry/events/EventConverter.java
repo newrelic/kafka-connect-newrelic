560ae7a19d169bd1b208fad5b3ba6e152071cfd7
Dan Stadler <dstadler@newrelic.com>
2021-05-25 16:10:19 +0000

    proposed changes for handling map-based records

--- newrelic-kafka-connector/src/main/java/com/newrelic/telemetry/events/EventConverter.java
+++ newrelic-kafka-connector/src/main/java/com/newrelic/telemetry/events/EventConverter.java
@@ -9,6 +9,9 @@ import org.apache.kafka.connect.sink.SinkRecord;
 
 
 import java.util.Optional;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.*;
 
 /**
  * utilities used to convert a single record to a New Relic Event
@@ -76,7 +79,63 @@ public class EventConverter {
         // check if the value in the record is a Map.  If not, throw https://kafka.apache.org/24/javadoc/org/apache/kafka/connect/errors/DataException.html
         // then convert the map to an EventModel
 
-        return null;
+	if (!(record.value() instanceof Map)) {
+            throw new DataException("value must be instance of Map");
+        }
+
+        Map recordMapValue = (Map)record.value();
+
+        String eventType = "";
+
+        if (!recordMapValue.containsKey(EVENT_TYPE_ATTRIBUTE)) {
+            throw new DataException(String.format("All records must contain a '%s' field", EVENT_TYPE_ATTRIBUTE));
+        } else {
+            eventType = recordMapValue.get(EVENT_TYPE_ATTRIBUTE).toString();
+        }
+
+        Attributes attributes = new Attributes();
+
+        Set<Map.Entry<String, Object>> entries = recordMapValue.entrySet();
+        entries.stream()
+            .filter(e -> !e.getKey().equals(EVENT_TYPE_ATTRIBUTE))
+            .forEach(e -> {
+
+                String key = e.getKey().toString();
+
+                if (e.getValue() instanceof String) {
+
+                    attributes.put(key, new String(e.getValue().toString()));
+
+                } else if (e.getValue() instanceof Number) {
+
+                    if (e.getValue() instanceof Float) {
+                        attributes.put(key, Float.valueOf(Float.parseFloat(e.getValue().toString())));
+                    }
+                    else if (e.getValue() instanceof Integer) {
+                        attributes.put(key, Integer.valueOf(Integer.parseInt(e.getValue().toString())));
+                    }
+
+                    // else if (e.getValue() instanceof Double) {
+                    //     attributes.put(key, Double.valueOf(Double.parseDouble(e.getValue().toString())));
+                    // }
+
+                    else {
+                        // handle all other cases as strings
+                        attributes.put(key, new String(e.getValue().toString()));
+                    }
+
+                } else {
+                    System.out.println("not writing attribute for: " + e.getKey().toString());
+                }
+
+            });
+
+        // create the event using the record's timestamp
+        //Event event = new Event(eventType, attributes, record.timestamp());
+        Event event = new Event(eventType, attributes, record.timestamp());
+
+        return event;
+
     }
 
 
