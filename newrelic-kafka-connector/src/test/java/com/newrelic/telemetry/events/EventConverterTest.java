94dab0f9c1e8792fc859d7f2cf57b2234facba76
Eric Mittelhammer <code@ericmittelhammer.com>
2021-05-25 17:22:50 -0400

    moving fixtures into their own class

--- newrelic-kafka-connector/src/test/java/com/newrelic/telemetry/events/EventConverterTest.java
+++ newrelic-kafka-connector/src/test/java/com/newrelic/telemetry/events/EventConverterTest.java
@@ -1,63 +1,21 @@
 package com.newrelic.telemetry.events;
 
 import com.newrelic.telemetry.Attributes;
-import com.newrelic.telemetry.events.EventConverter;
-import com.newrelic.telemetry.events.Event;
-import org.apache.kafka.common.record.TimestampType;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
-import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class EventConverterTest {
 
-
-
-    SinkRecord sampleStructRecord;
-
-    SinkRecord sampleSchemalessRecord;
-
-    Schema schema;
-
-    Struct recordStructValue;
-
-    Map<String, Object> recordMapValue;
+    Fixtures fixtures;
 
     @Before
     public void setUp() throws Exception {
-
-        schema = SchemaBuilder.struct()
-                .field("eventType", Schema.STRING_SCHEMA)
-                .field("stringField", Schema.STRING_SCHEMA)
-                .field("intField", Schema.INT32_SCHEMA)
-                .field("floatField", Schema.FLOAT32_SCHEMA)
-                .field("flattened.field.name", Schema.STRING_SCHEMA)
-                .build();
-
-        recordMapValue = new HashMap<>();
-        recordMapValue.put("eventType", "myTestEvent");
-        recordMapValue.put("stringField", "stringValue");
-        recordMapValue.put("intField", 10);
-        recordMapValue.put("floatField", 9.9f);
-        recordMapValue.put("flattened.field.name", "someStringValue");
-
-        recordStructValue = new Struct(schema);
-
-        // set the struct to all the same values as the map
-        recordMapValue.entrySet().forEach(e -> recordStructValue.put(e.getKey(), e.getValue()));
-
-        //create a test record using a schema
-        sampleStructRecord = new SinkRecord("myTopic", 0, null, null, schema, recordStructValue, 1001, 1621466257L, TimestampType.NO_TIMESTAMP_TYPE);
-
-        //create a test record without a schema
-        sampleSchemalessRecord = new SinkRecord("myTopic", 0, null, null, null, recordMapValue, 1001, 1621466257L, TimestampType.NO_TIMESTAMP_TYPE);
+        this.fixtures = new Fixtures();
     }
 
     void testEquals(Event createdEvent) {
@@ -82,7 +40,7 @@ public class EventConverterTest {
 
     @Test
     public void withSchema() {
-        Event testEvent = EventConverter.toNewRelicEvent(sampleStructRecord);
+        Event testEvent = EventConverter.toNewRelicEvent(this.fixtures.sampleStructRecord);
         testEquals(testEvent);
     }
 
